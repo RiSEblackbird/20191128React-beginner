0452b76d9bd7584c67378e799ed58d38def2bfa1
SELA <RiSEblackbird@users.noreply.github.com>
2019-11-30 20:32:17 +0900

    Squareクラスを関数コンポーネントに書き換え

--- 20191130/src/index.js
+++ 20191130/src/index.js
@@ -16,18 +16,12 @@ serviceWorker.unregister();
 */
 
 // tutorial code
-class Square extends React.Component {
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
   
 class Board extends React.Component {
